28de0f3449c6e2fb7551e2fa21bbb83abd0076ab
BigDataSamuli <samuli.holopainen@bigdatasolutions.fi>
2016-08-16 17:55:13 +0300

    Use external Express typings

--- src/transport/express_server.ts
+++ src/transport/express_server.ts
@@ -2,14 +2,14 @@
 //import {RpcBackend} from '../backend';
 import {JsonTransportBackend} from './json_transport';
 
-import * as express from '../external/express';
+import * as express from 'express';
 
 /**
  * Wrap a JSON transport backend inside a Express resolver function.
  */
 export function createExpressResolver(jsonBackend: JsonTransportBackend) {
     return async (request: express.Request, response: express.Response) => {
-        let payload = request.body;
+        let payload = request['body'];
         let responsePayload = await jsonBackend.handleJsonPayload(payload);
         response.json(responsePayload);
     }
